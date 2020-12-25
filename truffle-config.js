65ce381557555710b005d5dd9b786c4efd4242e6
tenet-dev-xu <wenweixu@drep.org>
2020-12-25 16:36:53 +0800

    update

--- truffle-config.js
+++ truffle-config.js
@@ -7,7 +7,6 @@ module.exports = {
   // You can also follow this format for other networks;
   // see <http://truffleframework.com/docs/advanced/configuration>
   // for more details on how to specify configuration options!
-  //
   networks: {
     development: {
       host: "127.0.0.1",
